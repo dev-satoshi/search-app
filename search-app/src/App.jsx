16ff1a4a7acf739b7663e9fde11eb1b7444bdf02
satoshi <dev.satoshi.1016@gmail.com>
2023-06-06 03:12:15 +0900

    add: 検索機能を実装

--- search-app/src/App.jsx
+++ search-app/src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ users, setUsers ] = useState([]);
+  const [ searchQuery, setSearchQuery ] = useState([]);
+  const ref = useRef();
+
+  const handleSearch = () => {
+    // フィルタリング機能
+    setSearchQuery(
+      users.filter((user) => user.name.toLowerCase().includes(ref.current.value))
+    )
+  };
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        return res.json();
+      }).then((data) => setUsers(data));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="main">
+        <h2>検索アプリ</h2>
+        <input type='text' ref={ref} onChange={() => handleSearch()} />
+        <div className="content">
+          {searchQuery.map((user) => (
+            <div className="box" key={user.id}>
+              <h3>{user.name}</h3>
+              <hr />
+              <p>{user.email}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
